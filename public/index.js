12d3160bafe616147be506931859d367a4c053bd
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-20 20:38:10 +1000

    adding if statement

--- public/index.js
+++ public/index.js
@@ -1,4 +1,13 @@
 // Dependencies and Variables
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((reg) => {
+        console.log('Service worker registered.', reg);
+      });
+  });
+}
+
 let transactions = [];
 let myChart;
 
