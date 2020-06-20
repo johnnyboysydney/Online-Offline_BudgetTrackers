33a71a091e970d0fabfc8afcf597d9f66ba71e30
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-20 20:55:51 +1000

    troubleshooting indexedb

--- public/db.js
+++ public/db.js
@@ -1,5 +1,63 @@
-// Dependencies and variables
-let db;
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
 
-// listen for app coming back online
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = ({ target }) => {
+  let db = target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = ({ target }) => {
+  db = target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  console.log("hiiiii" + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  console.log("hello beenish i'm a store");
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
+
 window.addEventListener("online", checkDatabase);
