8fd018015ebee3f8a2be7de9674266fd3a4cc1d1
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-06-20 19:14:12 +1000

    adding module and rules

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,20 @@ const config = {
         filename: "bundle.js"
     },
     mode: "development",
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-env"]
+              }
+            }
+          }
+        ]
+      },
     plugins: [
         new WebpackPwaManifest({
             // the name of the generated manifest file
